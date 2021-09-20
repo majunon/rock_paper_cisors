49222f38df5e5a30e058f81fdae1f3803922eb6f
majunon <fayardjul@gmail.com>
2021-09-20 17:04:41 +0200

    add the final score when a player reach 5 victories

--- script.js
+++ script.js
@@ -13,43 +13,60 @@ function computerPlay(){
 
 const buttons = document.querySelectorAll('button');
 
+let scorePlayer=0;
+let scoreComputer=0;
+
 buttons.forEach(button => button.addEventListener('click',function(e){
   let computerSelection = computerPlay();
   let result = document.querySelector("#result");
   let score = document.querySelector('#score');
+  let winner = document.querySelector('#winner');
 
   if(this.id==='rock'){
     if(computerSelection === 'rock'){
       result.textContent="It's a tie!";
     }
     else if(computerSelection === 'paper'){
-      result.textContent="You lose! Paper beats rock.";
+      result.textContent="You lose this round! Paper beats rock.";
+      scoreComputer++;
     }
     else {
-      result.textContent="You win! Rock beats scissors.";
+      result.textContent="You win this round! Rock beats scissors.";
+      scorePlayer++;
     }
   }
   else if(this.id==='paper'){
     if(computerSelection === 'rock'){
-      result.textContent="You win! Paper beats rock.";
+      result.textContent="You win this round! Paper beats rock.";
+      scorePlayer++;
     }
     else if(computerSelection === 'paper'){
       result.textContent="It's a tie!";
     }
     else {
-      result.textContent="You lose! Scissors beat paper.";
+      result.textContent="You lose this round! Scissors beat paper.";
+      scoreComputer++;
     }
   }
   else {
     if(computerSelection === 'rock'){
-      result.textContent="You lose! Rock beats scissors.";
+      result.textContent="You lose this round! Rock beats scissors.";
+      scoreComputer++;
     }
     else if(computerSelection === 'paper'){
-      result.textContent="You win! Scissors beat paper.";
+      result.textContent="You win this round! Scissors beat paper.";
+      scorePlayer++;
     }
     else {
       result.textContent="It's a tie!";
     }
+  }
+  score.textContent=`Player ${scorePlayer} -- ${scoreComputer} Computer`;
+  if (scorePlayer>=5){
+    winner.textContent="You win this match!!!";
+  }
+  else if(scoreComputer>=5){
+    winner.textContent="You lose this match !!!";
   }
 }));
 
